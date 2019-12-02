d09ef8ff36978d18fc1db5bfe4eb9b7cf25646b9
Peter Steiner <peter.steiner@svt.se>
2019-12-02 12:00:41 +0100

    d2b

--- d-2.py
+++ d-2.py
@@ -20,38 +20,52 @@ def readInput():
     file.close()
     return data
 
+def getOutput(intcodeprg):
+    pos = 0
+    while intcodeprg[pos] != 99:
+      #print("po: ", intcodeprg[pos])
+      cmd = intcodeprg[pos]
+      a = intcodeprg[intcodeprg[pos+1]]
+      b = intcodeprg[intcodeprg[pos+2]]
+      c = intcodeprg[pos+3]
+      # print("", a, b, c, len(intcodeprg))
+      if cmd == 1:
+          intcodeprg[c] = a + b
+      if cmd == 2:
+          intcodeprg[c] = (a*b)
+      pos += 4
+      # print("", intcodeprg)
+    return intcodeprg[0]
+
 def a():
-    intcodeprg = [int(n) for n in readInput().split(',')]
-    totsum = 0
-    pos = 0    
+    intcodeprg = [int(n) for n in readInput().split(',')]        
     intcodeprg[1] = 12
     intcodeprg[2] = 2
     
-    while intcodeprg[pos] != 99:
-        #print("po: ", intcodeprg[pos])
-        cmd = intcodeprg[pos]
-        a = intcodeprg[intcodeprg[pos+1]]
-        b = intcodeprg[intcodeprg[pos+2]]
-        c = intcodeprg[pos+3]
-        print("", a, b, c)
-        if cmd == 1:
-            print("1wtf", cmd)
-            intcodeprg[c] = a + b
-        if cmd == 2:
-            print("2wtf", cmd)
-            intcodeprg[c] = (a*b)
-        pos += 4
-        print("", intcodeprg)
-    print("A): ", intcodeprg[0])
+    answ = getOutput(intcodeprg)
+    print("A): ", answ)
 
 def b():
-    rows = [n for n in readInput().split('\n')]
-    totsum = 0
+    intcodeprg = [int(n) for n in readInput().split(',')]        
+    intcodeprg[1] = 12
+    intcodeprg[2] = 2
 
-    print("B): ", totsum)
+    for n in range(100):
+      for k in range(100):
+        intcodeprg[1] = n
+        intcodeprg[2] = k
+        try:
+          # print(n, k)
+          answ = getOutput(intcodeprg.copy())
+          # print("", answ)
+          if answ ==  19690720:
+            print("noun/verb", n, k, 100*n+k)          
+        except:
+          pass
+    # print("B): ", totsum)
 
 # Main body
 if __name__ == '__main__':
-    a()
-    #Sb()
+    # a()
+    b()
     sys.exit(1)
