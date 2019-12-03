1fbedc5c28cfa5bee5a5ddd8fedb73502862049f
Peter Steiner <peter.steiner.79.ps@gmail.com>
2019-12-03 22:09:46 +0100

    Another aspect of code hell, at least I dropped the global variables...

--- d-3.py
+++ d-3.py
@@ -21,121 +21,97 @@ def readInput():
     file.close()
     return data
 
-x = 1
-y = 1
-distance = 0
-crossing = 10000
-crossings = [0][0]
-stepx = 0
-stepy = 0
-minsteps_intercerption = 1000000
+class Wire:
+  def __init__(self,  id = 0, x = 1, y = 1, sx = 0, sy = 0):
+    self.id = id
+    self.x = x
+    self.y = y
+    self.stepx = sx
+    self.stepy = sy
+  
+  def move(self, direction):
+    if direction == "U":
+      self.y += 1
+      self.stepy += 1
+    if direction == "D":
+      self.y -= 1 
+      self.stepy += 1
+    if direction == "R": 
+      self.x += 1
+      self.stepx += 1
+    if direction == "L":
+      self.x -= 1 
+      self.stepx += 1 
 
-def step(dir, id, panel):
-  global x
-  global y
-  global stepx
-  global stepy
-  global crossing
-  global minsteps_intercerption
+  def print(self):
+    print('Wire', self.x, self.y, self.stepx, self.stepy)
 
-  if dir == "U":
-    y += 1
-    stepy += 1
-  if dir == "D":
-    y -= 1 
-    stepy += 1
-  if dir == "R": 
-    x += 1
-    stepx += 1
-  if dir == "L":
-    x -= 1 
-    stepx += 1
-    
-  crossings[x][y] += stepx+stepy
+class FrontPanel:
+  def __init__(self, a = 10000):
+    self.a = a
+    self.front_panel = [[0 for x in range(a)] for y in range(a)]  
+    self.front_panel[1][1] = 0
+    self.steps_per_cordinate = [[0 for x in range(a)] for y in range(a)]  
 
-  if panel[x][y] != 0:
-    if panel[x][y] != id:
-      #print(x, y, id)
-      if abs(x)+abs(y)-2 < crossing:
-        crossing = abs(x)+abs(y)-2
-        # print("manhattan dist:", x+y-2)
-      if crossings[x][y] < minsteps_intercerption:
-        minsteps_intercerption = crossings[x][y]
-        # print("Distance:", minsteps_intercerption)
-  panel[x][y] = id
+    self.manhattan_distance = 1000 
+    self.minsteps_interc = 1000000
+
+  def print(self):
+    print('Front panel', self.manhattan_distance, self.minsteps_interc)
+
+def isWireCrossing(wire, panel):
+  x, y = wire.x, wire.y
+  #Is there a wire at possition
+  if panel.front_panel[x][y] != 0:
+    #Its another wire  
+    return panel.front_panel[x][y] != wire.id
+  return False
+
+def step(wire, direction, panel):
+  wire.move(direction)
+  #wire.print()
+  x, y = wire.x, wire.y
+  # sum all steps for all cords hence +=
+  panel.steps_per_cordinate[x][y] += wire.stepx+wire.stepy
+ 
+  if isWireCrossing(wire, panel):
+    if abs(x)+abs(y)-2 < panel.manhattan_distance:
+      panel.manhattan_distance = abs(x)+abs(y)-2
+    if panel.steps_per_cordinate[x][y] < panel.minsteps_interc:
+      panel.minsteps_interc = panel.steps_per_cordinate[x][y]
+  
+  # Mark visited      
+  panel.front_panel[x][y] = wire.id
 
 
-def move(m, panel, id):
-    global x
-    global y 
-
-    move = 0
-    dir = "R"
-    m = re.match(r"([a-z]+)(\d+)", m, re.I)
+def move(wire, move, panel):
+    steps, dir = 0, "R"
+    m = re.match(r"([a-z]+)(\d+)", move, re.I)
     if m:
-        dir = m.group(1)
-        move = int(m.group(2))
-    for n in range(move):
-      step(dir, id, panel)     
+      dir = m.group(1)
+      steps = int(m.group(2))
+    for n in range(steps):
+      step(wire, dir, panel)     
+
+
+def a_b():
+    rows = [n for n in readInput().split('\n')]    
+    panel = FrontPanel()
+  
+    wire_id = 0
+    for row in rows:
+        wire_id += 1
+        wire = Wire(wire_id)  
+        print("Pull new wire", wire_id)    
+        for m in [n for n in row.split(',')]:
+          move(wire, m, panel)
     
-
-
-def a():
-    a = 10000
-    rows = [n for n in readInput().split('\n')]    
-    front_panel = [[0 for x in range(a)] for y in range(a)]  
-    global x
-    global y 
-    global stepx
-    global stepy
-    global crossing
-    global crossings
-
-    crossings = [[0 for x in range(a)] for y in range(a)]  
-    front_panel[x][y] = 1
-
-    id = int(0)
-    for row in rows:
-        x = 1
-        y = 1
-        stepx = 0
-        stepy = 0
-        id += 1
-        for m in [n for n in row.split(',')]:
-  #          print(m)
-            move(m, front_panel, id)
- #       print("cabel drawn")
-
-    print("A): ", crossing)
-
-def b():
-    a = 10000
-    rows = [n for n in readInput().split('\n')]    
-    front_panel = [[0 for x in range(a)] for y in range(a)]  
-    global x
-    global y 
-    global crossing
-    global crossings
-    global minsteps_intercerption
-
-    crossings = [[0 for x in range(a)] for y in range(a)]  
-    front_panel[x][y] = 1
-
-    id = 0
-    for row in rows:
-        x = 1
-        y = 1
-        stepx = 0
-        stepy = 0
-        id += 1
-        for m in [n for n in row.split(',')]:
-            move(m, front_panel, id)
-
-    print("Distance:", minsteps_intercerption)
+    #panel.print()
+    print("A): Manhattan distance: ", panel.manhattan_distance)
+    print("B): Least steps to interception:", panel.minsteps_interc)
 
 
 # Main body
 if __name__ == '__main__':
-    a()
-    b()
+    a_b()
     sys.exit(1)
