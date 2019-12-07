f4706b0b5402e417127d61f0d0976cdc4797190e
Peter Steiner <peter.steiner.79.ps@gmail.com>
2019-12-07 15:39:05 +0100

    Prep D7B

--- d-7b.py
+++ d-7b.py
@@ -13,8 +13,8 @@ import time
 import itertools
 
 # Global variables
-#task="d-7.test"
-task="d-7"
+task="d-7.test"
+#task="d-7"
 infile=task + ".input"
 
 def readInput():
@@ -31,6 +31,7 @@ def getValue(mode, pos, instruction):
 
 def amp(input, instruction):
     pos = 0
+    #print("Amp phases: ", input)
     input_index = 0
     while instruction[pos] != 99:
       cmd_tmp = [int(d) for d in str(instruction[pos])][::-1]
@@ -38,15 +39,21 @@ def amp(input, instruction):
       for i in range(len(cmd_tmp)): 
           cmd[i] = cmd_tmp[i]
       cmd = cmd[::-1]
+      op = 0
       OP, C, B, A = 10*cmd[3] + cmd[4], cmd[2], cmd[1], cmd[0]
       if (OP == 1 or OP == 2):        
+        # print("handle OP 1|2", OP)
+        # print(instruction[pos:pos+4])
         a = getValue(C, pos+1, instruction)
         b = getValue(B, pos+2, instruction)
         c = instruction[pos+3]
         if OP == 1:
+          # print("a + b @ c", a, b, c)
           instruction[c] = a + b
         if OP == 2:
+          # print("a X b @ c", a, b, c)
           instruction[c] = (a*b)
+        # print("new value at", c, instruction[c])
         pos += 4
 
 
@@ -94,6 +101,7 @@ def amp(input, instruction):
 def calc_thruster_power(phase_setting_sequences, instruction):
   maxAmp = 0
   amps = ["A", "B", "C", "D", "E"]
+  state = []
   for ph in phase_setting_sequences:
     phase_setting = [ph[0],0]
     thruster_power = amp(phase_setting, instruction.copy())
@@ -103,7 +111,40 @@ def calc_thruster_power(phase_setting_sequences, instruction):
       if thruster_power[-1] > maxAmp:
         maxAmp = thruster_power[-1]
         
+
   return maxAmp
+
+def calc_thruster_power_feedbackloop(phase_setting_sequences, instruction):
+  maxAmp = 0
+  amps = ["A", "B", "C", "D", "E"]
+  amplififiers = []
+  for i in range(5):
+    amplififiers.append(instruction.copy())
+
+  for ph in phase_setting_sequences:
+      phase_setting = [ph[0],0]
+      thruster_power = amp(phase_setting, amplififiers[0])
+      # First run
+      for i in range(1, 5):
+        thruster_power = amp([ph[i], thruster_power[-1]], amplififiers[i])
+        if thruster_power[-1] > maxAmp:
+          maxAmp = thruster_power[-1]
+      # Feedback loop
+      run = True
+      while run:
+        for i in range(5):
+          #input = thruster_power[-1]
+          thruster_power = amp([ph[i], thruster_power[-1]], amplififiers[i])
+          if (amps[i] == "E" and thruster_power[-1] > maxAmp):
+            maxAmp = thruster_power[-1]
+            print("Setting max thruster power: ", maxAmp)
+          if (len(thruster_power) == 1 and thruster_power[0] == 1337):
+            print("Finished thruster power feedback loop @", amps[i])
+            run = False
+            break
+
+  return maxAmp
+
   
 def test():
     
@@ -124,8 +165,18 @@ def a():
   
   print("A): Thruster power", thruster_power)
 
+
+def b():
+  phase_setting_sequences = list(itertools.permutations([i for i in range(5, 10)]))
+  #print(phase_setting_sequences[0])
+  instruction = [int(n) for n in readInput().split(',')]    
+  thruster_power = calc_thruster_power_feedbackloop(phase_setting_sequences, instruction)
+  
+  print("A): Thruster power", thruster_power)
+
 # Main body
 if __name__ == '__main__':
     test()
-    a()
+    #a()
+    b()
     sys.exit(1)
